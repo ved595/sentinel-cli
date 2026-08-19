2ebbfaf544db21a5a844823fdceeb76992ed64f0
veshetty <veshetty@iu.edu>
2026-08-19 07:50:09 -0400

    Add file integrity checking

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,23 +17,103 @@ func main() {
 	command := os.Args[1]
 
 	if command == "hash" {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: sentinel hash <filename>")
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: sentinel hash <filename>")
+			return
+		}
+
+		filename := os.Args[2]
+
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+
+		hash := sha256.Sum256(data)
+
+		fmt.Println("File:", filename)
+		fmt.Println("SHA-256:", hex.EncodeToString(hash[:]))
+
 		return
 	}
 
-	filename := os.Args[2]
+	if command == "baseline" {
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: sentinel baseline <filename>")
+			return
+		}
 
-data, err := os.ReadFile(filename)
-if err != nil {
-	fmt.Println("Error reading file:", err)
-	return
-}
+		filename := os.Args[2]
 
-hash := sha256.Sum256(data)
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
 
-fmt.Println("File:", filename)
-fmt.Println("SHA-256:", hex.EncodeToString(hash[:]))
+		hash := sha256.Sum256(data)
+		hashString := hex.EncodeToString(hash[:])
+
+		baseline := filename + ":" + hashString
+
+		err = os.WriteFile(
+			"sentinel-baseline.txt",
+			[]byte(baseline),
+			0644,
+		)
+		if err != nil {
+			fmt.Println("Error saving baseline:", err)
+			return
+		}
+
+		fmt.Println("Baseline created for:", filename)
+		return
+	}
+
+	if command == "check" {
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: sentinel check <filename>")
+			return
+		}
+
+		filename := os.Args[2]
+
+		baselineData, err := os.ReadFile("sentinel-baseline.txt")
+		if err != nil {
+			fmt.Println("Error reading baseline:", err)
+			return
+		}
+
+		parts := strings.SplitN(string(baselineData), ":", 2)
+
+		if len(parts) != 2 {
+			fmt.Println("Invalid baseline file")
+			return
+		}
+
+		savedFilename := parts[0]
+		savedHash := parts[1]
+
+		if savedFilename != filename {
+			fmt.Println("No baseline found for:", filename)
+			return
+		}
+
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+
+		currentHash := sha256.Sum256(data)
+		currentHashString := hex.EncodeToString(currentHash[:])
+
+		if currentHashString == savedHash {
+			fmt.Println("File integrity verified:", filename)
+		} else {
+			fmt.Println("WARNING: File has been modified:", filename)
+		}
 
 		return
 	}
